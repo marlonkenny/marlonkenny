1d953fd711f8d720c89af95bbbf6d5772d109ab3
Marlon Kenny <mkenny@adobe.com>
2023-12-18 09:54:43 -0800

    Switch to Next

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+      <p>Hey! I'm Marlon Kenny.</p>
+      <p>I'm a software developer at <a href="https://frame.io">Frame.io</a></p>
+
+      <p className="mt-12"><a href="https://www.linkedin.com/in/marlonkenny/">Contact Me</a></p>
+    </main>
+  )
+}
