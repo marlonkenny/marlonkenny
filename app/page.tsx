5791c68b3ab672d59dd7a3c9e45c6314a80b427a
Marlon Kenny <mkenny@adobe.com>
2023-12-18 09:59:48 -0800

    Fix apostrophes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <p>Hey! I'm Marlon Kenny.</p>
-      <p>I'm a software developer at <a href="https://frame.io">Frame.io</a></p>
+      <p>Hey! I&apos;m Marlon Kenny.</p>
+      <p>I&apos;m a software developer at <a href="https://frame.io">Frame.io</a></p>
 
       <p className="mt-12"><a href="https://www.linkedin.com/in/marlonkenny/">Contact Me</a></p>
     </main>
